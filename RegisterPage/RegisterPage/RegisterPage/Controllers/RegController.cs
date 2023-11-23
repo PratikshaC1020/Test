889220fb27dcc7db40d2815811a69403e61b4fae
Pratiksha <chavanpp6800@gmail.com>
2023-11-23 20:55:26 +0530

    first commit

---
+++ RegisterPage/RegisterPage/RegisterPage/Controllers/RegController.cs
@@ -0,0 +1,35 @@
+﻿using RegisterPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace RegisterPage.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SignIn(Register reg)
+        {
+            string FirstName = reg.Firstname;
+            string LastName = reg.Lastname;
+            string Email = reg.Email;
+            string Password = reg.Password;
+            string confirmPassword = reg.confirmPassword;
+            bool ShowSubscribe=reg.ShowSubscribe;
+            var result =  "Form submitted Successfully";
+            return Json(result, JsonRequestBehavior.AllowGet);
+           
+        }
+    }
+}
---
+++ RegisterPage/RegisterPage/RegisterPage/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegisterPage.Models
+{
+    public class Register
+    {
+        public string Firstname { get; set; }   
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string confirmPassword { get; set; }
+        public bool ShowSubscribe { get; set; }
+    }
+}
